933a90d7807ce7f3c2a347332b03cb024aa63c90
dev-remun <raymond.cabarlo.balingbing@gmail.com>
2026-09-09 21:42:02 +0800

    feat: added the header file that will contains all of the modules for perx

---
+++ src/perx_modules.h
@@ -0,0 +1,11 @@
+#ifndef PERX_MODULES_H
+#define PERX_MODULES_H
+
+/**
+ * a read-eval-print-loop that read an input, evaluate it,
+ * then print the result of the input or the evaluation.
+ * This is the core of a shell
+ */
+void repl();
+
+#endif
