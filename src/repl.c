d291e149b2ddb5562dab2d6f5d920af0c41c7460
dev-remun <raymond.cabarlo.balingbing@gmail.com>
2026-09-09 22:08:24 +0800

    feat: added a temporary implementation for repl  - for the meantime this rejects all possible string input as    a command except for 'exit' which exits perx repl  - (todo) fix the input size to make it dynamic based on user input

--- src/repl.c
+++ src/repl.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "perx_modules.h"
 
+/**
+ * Implements the read-evaluate-print-loop (REPL) for the perx shell
+ * it continuously asks for user input and handles the execution
+ */
 void repl() {
-    printf("repl\n");
+    
+    // # start the continuous loop for the shell
+    do {
+
+        // # disable output buffering to show prompt immediately
+        setbuf(stdout, NULL);
+
+        // # display the shell prompt
+        printf("(perx) $ ");
+
+        // # array to hold the user input
+        char input[1024];
+        
+        // # get the input typed by the user
+        fgets(input, sizeof(input), stdin);
+        
+        // # remove the new line character at the end of the string
+        input[strcspn(input, "\n")] = '\0';
+
+        if(strcmp(input, "exit") == 0) {
+            printf("exited from perx shell\n");
+            return;
+        }
+
+        // # temporarily print command not found for all inputs
+        printf("%s: command not found\n\n", input);
+
+    } while(1);
+
 }
